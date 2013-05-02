22f8f1549ea76915a5db53df0b6536eab070f1c1
Trent Monahan <sinz@d3s.co>
2013-05-02 18:18:54 +1000

    Changed how downloading minecraft.jar works
    Also deleted old LWJGL downloading, because the new method is better.

--- SinZ MC Launcher/Download/VannilaMinecraft.cs
+++ SinZ MC Launcher/Download/VannilaMinecraft.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,27 +13,48 @@ namespace SinZ_MC_Launcher.Download {
     class DownloadMinecraft {
 
         Boolean isDownloading = true;
-        
-        public DownloadMinecraft(MainForm form, String version, String path) {
+        String path;
+        String version;
+        public DownloadMinecraft(MainForm form, String version, String path, Boolean New = true) {
+            this.path = path;
+            this.version = version;
+            if (New) {
+                Thread thread = new Thread(new ThreadStart(DownloadNewMinecraft));
+                thread.Start();
+                while (thread.IsAlive) {
+                    Application.DoEvents();
+                }
+            }
+            else {
+                Thread thread = new Thread(new ThreadStart(DownloadOldMinecraft));
+                thread.Start();
+                while (thread.IsAlive) {
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        public void DownloadOldMinecraft() {
             String URL = String.Format("http://assets.minecraft.net/{0}/minecraft.jar", version);
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
 
             WebClient client = new WebClient();
             client.DownloadProgressChanged += SetProgress;
-            client.DownloadFileCompleted += Downloaded;
-            client.DownloadFileAsync(new Uri(URL), path + "minecraft.jar");
-            while (isDownloading) {
-                Application.DoEvents();
-            }
+            client.DownloadFile(URL, path + "minecraft.jar");
+        }
+        public void DownloadNewMinecraft() {
+            String URL = String.Format("https://s3.amazonaws.com/Minecraft.Download/versions/{0}/{1}.jar", version);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            WebClient client = new WebClient();
+            client.DownloadProgressChanged += SetProgress;
+            client.DownloadFile(URL, path + version+".jar");
         }
 
         private void SetProgress(object sender, DownloadProgressChangedEventArgs e) {
             //TODO: Do hackery to get back to MainForm without sending MainForm
-        }
-
-        public void Downloaded(object sender, AsyncCompletedEventArgs e) {
-            isDownloading = false;
         }
     }
 }
