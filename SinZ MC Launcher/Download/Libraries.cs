7e15df5dbe09bbec3892c95f376ad45e436ccf61
Trent Monahan <sinz@d3s.co>
2013-05-03 10:28:06 +1000

    Added Library downloading

--- SinZ MC Launcher/Download/Libraries.cs
+++ SinZ MC Launcher/Download/Libraries.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -11,16 +14,81 @@ namespace SinZ_MC_Launcher.Download {
     class Libraries {
 
         public Uri DOWNLOAD_LINK = new Uri("https://s3.amazonaws.com/Minecraft.Download/");
+        String location = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".sinzmc/");
+        private string version;
+
+        private List<String> results = new List<string>();
 
         public Libraries(String version) {
+            this.version = version;
+            Thread queryThread = new Thread(new ThreadStart(QueryLibraryServer));
+            queryThread.Start();
+            while (queryThread.IsAlive)
+            {
+                Application.DoEvents();
+            }
+            Thread downloadThread = new Thread(new ThreadStart(DownloadLibraries));
+            downloadThread.Start();
+            while (downloadThread.IsAlive)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void DownloadLibraries()
+        {
             WebClient client = new WebClient();
-            String versionInfo = client.DownloadString(DOWNLOAD_LINK + "versions/" + version + "/" + version + ".json");
+
+            foreach (String result in results)
+            {
+                if (!File.Exists(Path.Combine(location, "libraries", result)))
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(Path.Combine(location, "libraries", result))))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(Path.Combine(location, "libraries", result)));
+                    }
+                    Console.WriteLine(result);
+                    client.DownloadFile(DOWNLOAD_LINK +"libraries/"+ result.Replace('\\','/'), Path.Combine(location, "libraries", result));
+                }
+            }
+        }
+        public void QueryLibraryServer()
+        {
+            WebClient client = new WebClient();
+            String versionInfo = client.DownloadString(DOWNLOAD_LINK + "versions" + Path.DirectorySeparatorChar + version + Path.DirectorySeparatorChar + version + ".json");
             Dictionary<String, object> output = deserializeToDictionary(versionInfo);
 
             List<String> libraries = new List<String>();
-            foreach (JObject library in (JArray)output["libraries"]) {
-                Console.WriteLine(library["name"].ToString());
+            List<String> natives = new List<string>();
+            foreach (JObject library in (JArray)output["libraries"])
+            {
                 libraries.Add(library["name"].ToString());
+                try
+                {
+                    if (library["natives"].ToString().Length > 0)
+                    {
+                        natives.Add(library["name"].ToString());
+                        Console.WriteLine("It is natives?" + library["name"]);
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    //it isn't a native...
+                }
+            }
+
+            foreach (String library in libraries)
+            {
+                String[] a = library.Split(':');
+                a[0] = a[0].Replace('.', '/');
+                if (natives.Contains(library))
+                {
+                    results.Add(a[0] + Path.DirectorySeparatorChar + a[1] + Path.DirectorySeparatorChar + a[2] + Path.DirectorySeparatorChar + a[1] + "-" + a[2] + "-natives-windows.jar");
+                }
+                else
+                {
+                    results.Add(a[0] + Path.DirectorySeparatorChar + a[1] + Path.DirectorySeparatorChar + a[2] + Path.DirectorySeparatorChar + a[1] + "-" + a[2] + ".jar");
+                }
             }
         }
 
